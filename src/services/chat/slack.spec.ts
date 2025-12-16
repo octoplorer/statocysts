bb062abe9b83ec1144442d86d312af5d399d34c6
enpitsulin <enpitsulin@gmail.com>
2025-12-16 17:48:25 +0800

    test: remove seem like secrets

--- src/services/chat/slack.spec.ts
+++ src/services/chat/slack.spec.ts
@@ -5,13 +5,13 @@ describe('slack', () => {
   describe('bot API format', () => {
     it('should build a request with Bot API token', async () => {
       const request = buildSlackRequest(
-        new URL('slack://xoxb:123456789012-1234567890123-4mt0t4l1YL3g1T5L4cK70k3N@C001CH4NN3L'),
+        new URL('slack://xoxb:123456789012-1234567890123-XXXXXXXXXXXXXXXXXXXXXXXX@C001CH4NN3L'),
         'Hello, world!',
       )
 
       expect(request.method).toBe('POST')
       expect(request.url).toBe('https://slack.com/api/chat.postMessage')
-      expect(request.headers.get('Authorization')).toBe('Bearer xoxb-123456789012-1234567890123-4mt0t4l1YL3g1T5L4cK70k3N')
+      expect(request.headers.get('Authorization')).toBe('Bearer xoxb-123456789012-1234567890123-XXXXXXXXXXXXXXXXXXXXXXXX')
       expect(request.headers.get('Content-Type')).toBe('application/json')
 
       const body = await request.json()
@@ -23,7 +23,7 @@ describe('slack', () => {
 
     it('should build a request with botname parameter', async () => {
       const request = buildSlackRequest(
-        new URL('slack://xoxb:123456789012-1234567890123-4mt0t4l1YL3g1T5L4cK70k3N@C001CH4NN3L?botname=TestBot'),
+        new URL('slack://xoxb:123456789012-1234567890123-XXXXXXXXXXXXXXXXXXXXXXXX@C001CH4NN3L?botname=TestBot'),
         'Hello, world!',
       )
 
@@ -37,7 +37,7 @@ describe('slack', () => {
 
     it('should build a request with icon emoji parameter', async () => {
       const request = buildSlackRequest(
-        new URL('slack://xoxb:123456789012-1234567890123-4mt0t4l1YL3g1T5L4cK70k3N@C001CH4NN3L?icon=%3Arobot_face%3A'),
+        new URL('slack://xoxb:123456789012-1234567890123-XXXXXXXXXXXXXXXXXXXXXXXX@C001CH4NN3L?icon=%3Arobot_face%3A'),
         'Hello, world!',
       )
 
@@ -51,7 +51,7 @@ describe('slack', () => {
 
     it('should build a request with icon URL parameter', async () => {
       const request = buildSlackRequest(
-        new URL('slack://xoxb:123456789012-1234567890123-4mt0t4l1YL3g1T5L4cK70k3N@C001CH4NN3L?icon=https%3A%2F%2Fexample.com%2Ficon.png'),
+        new URL('slack://xoxb:123456789012-1234567890123-XXXXXXXXXXXXXXXXXXXXXXXX@C001CH4NN3L?icon=https%3A%2F%2Fexample.com%2Ficon.png'),
         'Hello, world!',
       )
 
@@ -65,7 +65,7 @@ describe('slack', () => {
 
     it('should build a request with color parameter', async () => {
       const request = buildSlackRequest(
-        new URL('slack://xoxb:123456789012-1234567890123-4mt0t4l1YL3g1T5L4cK70k3N@C001CH4NN3L?color=good'),
+        new URL('slack://xoxb:123456789012-1234567890123-XXXXXXXXXXXXXXXXXXXXXXXX@C001CH4NN3L?color=good'),
         'Hello, world!',
       )
 
@@ -83,7 +83,7 @@ describe('slack', () => {
 
     it('should build a request with title parameter', async () => {
       const request = buildSlackRequest(
-        new URL('slack://xoxb:123456789012-1234567890123-4mt0t4l1YL3g1T5L4cK70k3N@C001CH4NN3L?title=Test+Title'),
+        new URL('slack://xoxb:123456789012-1234567890123-XXXXXXXXXXXXXXXXXXXXXXXX@C001CH4NN3L?title=Test+Title'),
         'Hello, world!',
       )
 
@@ -101,7 +101,7 @@ describe('slack', () => {
 
     it('should build a request with thread_ts parameter', async () => {
       const request = buildSlackRequest(
-        new URL('slack://xoxb:123456789012-1234567890123-4mt0t4l1YL3g1T5L4cK70k3N@C001CH4NN3L?thread_ts=1234567890.123456'),
+        new URL('slack://xoxb:123456789012-1234567890123-XXXXXXXXXXXXXXXXXXXXXXXX@C001CH4NN3L?thread_ts=1234567890.123456'),
         'Hello, world!',
       )
 
@@ -115,7 +115,7 @@ describe('slack', () => {
 
     it('should build a request with multiple parameters', async () => {
       const request = buildSlackRequest(
-        new URL('slack://xoxb:123456789012-1234567890123-4mt0t4l1YL3g1T5L4cK70k3N@C001CH4NN3L?botname=TestBot&icon=%3Arobot_face%3A&color=warning&title=Alert'),
+        new URL('slack://xoxb:123456789012-1234567890123-XXXXXXXXXXXXXXXXXXXXXXXX@C001CH4NN3L?botname=TestBot&icon=%3Arobot_face%3A&color=warning&title=Alert'),
         'Hello, world!',
       )
 
