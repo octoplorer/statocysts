42f8aeb8ee5437b02d75863a4872c601b7c767ab
enpitsulin <enpitsulin@gmail.com>
2025-12-17 15:39:58 +0800

    feat: add custom json notification service

---
+++ src/services/specialized/json/README.md
@@ -0,0 +1,5 @@
+## URL Scheme Format
+
+```
+json://hostname[:port]/path[?query]
+```
---
+++ src/services/specialized/json/index.ts
@@ -0,0 +1,47 @@
+import { defineProvider } from '#/core/provider'
+
+export const jsonProvider = defineProvider('json:', {
+  extractor: (url) => {
+    return Object.fromEntries(url.searchParams.entries())
+  },
+  parser: () => ({}) as unknown,
+  createRequest() {
+    const url = new URL(this.url)
+
+    const headers = new Headers([
+      ['Content-Type', 'application/json'],
+    ])
+
+    const body: Record<string, string> = {
+      message: this.message,
+    }
+
+    Array.from(url.searchParams.entries()).forEach(([key, value]) => {
+      if (key.startsWith(' ')) {
+        // `+` prefix which will be encoded as ` ` is for headers, remove it on searchParams and add to headers
+        url.searchParams.delete(key)
+        const headerKey = key.slice(1)
+        if (headers.has(headerKey)) {
+          headers.set(headerKey, value)
+        }
+        else {
+          headers.append(headerKey, value)
+        }
+      }
+      else if (key.startsWith(':')) {
+        // `:` prefix is for body properties, remove it on searchParams and add to body
+        url.searchParams.delete(key)
+        const propertyKey = key.slice(1)
+        body[propertyKey] = value
+      }
+    })
+
+    const requestUrl = url.toString().replace('json:', 'https:')
+
+    return new Request(requestUrl, {
+      method: 'POST',
+      body: JSON.stringify(body),
+      headers
+    })
+  },
+})
