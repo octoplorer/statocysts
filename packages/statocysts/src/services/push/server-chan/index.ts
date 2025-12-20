4669b3dfcff3262966ad76933abbf19a556897a1
enpitsulin <enpitsulin@gmail.com>
2025-12-20 21:53:00 +0800

    refactor: remove implement

--- packages/statocysts/src/services/push/server-chan/index.ts
+++ packages/statocysts/src/services/push/server-chan/index.ts
@@ -1,17 +1,12 @@
-import { defineProvider } from '#/shared'
+import type { FetchOptions } from 'ofetch'
+import { defineProvider, http } from '#/shared'
 
 export const serverChan = defineProvider('server-chan:', {
-  createRequest(ctx) {
-    const url = new URL(`${ctx.url.pathname}.send`, 'https://sctapi.ftqq.com')
-    return new Request(url, {
-      method: 'POST',
-      headers: {
-        'Content-type': 'application/json',
-      },
-      body: JSON.stringify({
-        // title: ctx.title, // TODO: need have title
-        desp: ctx.message,
-      }),
-    })
+  transport: http,
+  defaultOptions: {
+    fetchOptions: {} as FetchOptions,
+  },
+  prepare: async () => {
+    throw new Error('Not implemented')
   },
 })
