0ffe2505b021437809b5b7e4e22e4e505af7b0eb
enpitsulin <enpitsulin@gmail.com>
2026-07-29 12:05:01 +0800

    fix: project tsconfig

--- packages/statocysts/vitest.config.ts
+++ packages/statocysts/vitest.config.ts
@@ -1,12 +1,6 @@
-import { fileURLToPath } from 'node:url'
 import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
-  resolve: {
-    alias: {
-      '#': fileURLToPath(new URL('./src', import.meta.url)),
-    },
-  },
   test: {
     include: ['tests/**/*.spec.ts', 'src/**/*.spec.ts'],
     testTimeout: 30 * 1000,
